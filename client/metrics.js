ffda89c6eda24008d4e9a34cf040bb911b44d59e
Per Ploug <perp@spotify.com>
2022-10-04 15:52:31 +0200

    includes better pr/commit/work metrics

--- client/metrics.js
+++ client/metrics.js
@@ -37,6 +37,13 @@ module.exports = class Metrics extends Base {
       commit_total: Sequelize.INTEGER,
       commit_recent: Sequelize.INTEGER,
       commit_internal_total: Sequelize.INTEGER,
+      commit_internal_recent: Sequelize.INTEGER,
+
+      commit_work: Sequelize.INTEGER,
+      commit_nonwork: Sequelize.INTEGER,
+      commit_work_recent: Sequelize.INTEGER,
+      commit_nonwork_recent: Sequelize.INTEGER,
+
       committer_internal: Sequelize.INTEGER,
       committer_external: Sequelize.INTEGER,
     };
@@ -54,6 +61,13 @@ module.exports = class Metrics extends Base {
       commit_total: 'commit_total',
       commit_recent: 'commit_recent',
       commit_internal_total: 'commit_internal_total',
+      commit_internal_recent: 'commit_internal_recent',
+
+      commit_work: 'commit_work',
+      commit_nonwork: 'commit_nonwork',
+      commit_work_recent: 'commit_work_recent',
+      commit_nonwork_recent: 'commit_nonwork_recent',
+
       committer_internal: 'committer_internal',
       committer_external: 'committer_external',
       repository_id: 'repository_id',
@@ -70,7 +84,7 @@ module.exports = class Metrics extends Base {
   }
 
   async getAll() {
-    const query = `SELECT "Repository".id as repository_id, "Repository".stars, "Repository".forks, 
+    const query = `SELECT "Repository".id as repository_id, "Repository".stars, "Repository".forks,
     (select count(1) from "Issue" where "Issue".repository_id = "Repository".id) as issue_total,
     (select count(1) from "Issue" where "Issue".repository_id = "Repository".id and "Issue".state = 'open') as issue_open,
     (select count(1) from "Issue" where "Issue".repository_id = "Repository".id and "Issue".state = 'open' and date_part('day', (now() - "Issue".updated_at)) > 180 ) as issue_stale,
@@ -81,6 +95,11 @@ module.exports = class Metrics extends Base {
     (select count(1) from "Commit"  where "Commit".repository_id = "Repository".id) as commit_total,
     (select count(1) from "Commit"  where "Commit".repository_id = "Repository".id and date_part('day', (now() - "Commit".author_date)) < 90) as commit_recent,
     (select count(1) from "Commit" inner join "Member" on "Member".id = "Commit".author where "Commit".repository_id = "Repository".id) as commit_internal_total,
+    (select count(1) from "Commit" inner join "Member" on "Member".id = "Commit".author where "Commit".repository_id = "Repository".id and date_part('day', (now() - "Commit".author_date)) < 90) as commit_internal_recent,
+    (select count(1) from "Commit" inner join "Member" on "Member".id = "Commit".author where "Commit".repository_id = "Repository".id and (extract(isodow from "Commit".author_date) > 5 or extract(hour from "Commit".author_date) not between 8 and 17)) as commit_nonwork,
+    (select count(1) from "Commit" inner join "Member" on "Member".id = "Commit".author where "Commit".repository_id = "Repository".id and (extract(isodow from author_date) < 6 and extract(hour from author_date) between 8 and 17)) as commit_work,
+    (select count(1) from "Commit" inner join "Member" on "Member".id = "Commit".author where "Commit".repository_id = "Repository".id and date_part('day', (now() - "Commit".author_date)) < 90 and (extract(isodow from "Commit".author_date) > 5 or extract(hour from "Commit".author_date) not between 8 and 17)) as commit_nonwork_recent,
+    (select count(1) from "Commit" inner join "Member" on "Member".id = "Commit".author where "Commit".repository_id = "Repository".id and date_part('day', (now() - "Commit".author_date)) < 90 and (extract(isodow from author_date) < 6 and extract(hour from author_date) between 8 and 17)) as commit_work_recent,
     (select count( DISTINCT author) from "Commit" inner join "Member" on "Member".id = "Commit".author where "Commit".repository_id = "Repository".id and "Member".login is not null) as committer_internal,
     (select count( DISTINCT author) from "Commit" left join "Member" on "Member".id = "Commit".author where "Commit".repository_id = "Repository".id and "Member".login is null) as committer_external
     FROM "Repository" `;
